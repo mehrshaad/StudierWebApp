c88cebdde899dd18f41bb4578884af8054f8a1cb
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2021-09-26 09:31:23 +0330

    updated

--- App/models.py
+++ App/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class ToDoList(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Item(models.Model):
+    toDoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    text = models.CharField(max_length=20)
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return str(self.text)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Planning_WebApp
+## Planning_WebApp
+open cmd and enter:\
+```python manage.py runserver```\
+then copy the local server address into your browser.
