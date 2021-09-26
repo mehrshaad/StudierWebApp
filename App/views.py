cddd24b7ed4345d14b3f888c76fc87735efe212a
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2021-09-26 15:38:25 +0330

    updated

--- App/views.py
+++ App/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest
+from datetime import datetime
 
 
-# Create your views here.
-def index(response):
-    return HttpResponse('<h1>mehrshad</h1>')
+def index(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(request, 'index.html', {
+        'title': 'Home Page',
+        'year': datetime.now().year,
+    })
+
+
+# def contact(request):
+#     """Renders the contact page."""
+#     assert isinstance(request, HttpRequest)
+#     return render(
+#         request, 'contact.html', {
+#             'title': 'Contact',
+#             'message': 'Your contact page.',
+#             'year': datetime.now().year,
+#         })
+
+# def about(request):
+#     """Renders the about page."""
+#     assert isinstance(request, HttpRequest)
+#     return render(
+#         request, 'about.html', {
+#             'title': 'About',
+#             'message': 'Your application description page.',
+#             'year': datetime.now().year,
+#         })
